5f2f16eb1f47f58cf3f4b0cf8ac66f129a786630
mkkm88 <mohdkasimkhan1999@gmail.com>
2025-06-19 17:19:47 +0530

    New text file added with some text code

---
+++ lib/NewText.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class NewText extends StatelessWidget {
+  const NewText({super.key});
+  @override
+  Widget build(context) {
+    return Text("Hello dear");
+  }
+}
